93448933181e37549c8f3fd3481d431ca78b3d33
Simon <monkeys012345@gmail.com>
2025-08-14 16:20:30 -0400

    Updated config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyAqjoaV8sNnIzh2MtnUoJIhsHl1boRbDUs",
+    authDomain: "pearl-io.firebaseapp.com",
+    projectId: "pearl-io",
+    storageBucket: "pearl-io.firebasestorage.app",
+    messagingSenderId: "1047722086355",
+    appId: "1:1047722086355:web:5e784f1b1c17e55c506926"
+};
+
